917b9c5f35bc623ae3232cd2774ca08e2bce246d
Computhink-CS141 <cs141@computhink.com.sg>
2026-01-25 04:26:02 +0000

    end of lesson three

--- CT06_03/lesson3.py
+++ CT06_03/lesson3.py
@@ -1,1 +1,78 @@
-print("Hello from lesson 3")
+print("My sentence")
+
+age = 11
+print("age")
+print(age)
+
+x = 10
+age = age + x
+print(age)
+print("when i am 21yo, i want to continue studying")
+
+
+variable1 = 3
+variable2 = 5
+print(variable1 + variable2)
+print(variable1 - variable2)
+print(variable1 / variable2)
+print(variable1 * variable2)
+#  ** exponential 10 to the power of 3 is 30
+
+print("_________________________________")
+
+print(round(45.1234, 3))
+print("---------------------------------")
+print(round(4.567, 3))
+print(round(45.567, 2))
+print(round(45.567, 1))
+print(round(45.567, 0))
+print(round(45.567, -1))
+print(round(45.567, -2))
+print(round(7.5))
+print(round(8.5))
+
+print("--------------------------------")
+
+print(round(1785.956, -1))
+
+print("..........")
+print("4a:")
+
+a = 34
+b = 35
+print(a + b)
+print("..........")
+print("4b")
+
+print((50 - 10) * 8)
+print("..........")
+print("4c")
+
+print(100 /4)
+print("..........")
+print("4d")
+
+print((3 + 4) * 5 - (10 /2))
+
+
+
+print("..............--------")
+
+print("5a:")
+print(2 * 15)
+print(58)
+
+print("...............")
+# seafood_p = 96
+# side_dish = 18
+# rice = 2
+# side_dish = 18 * 3
+# print(side_dish)
+# rice = 2 * 6
+# print(rice)
+# print(seafood_p + side_dish + rice)
+total = 96 + (3*18) + (6 * 2)
+service_charge = 10/100 * total
+total = total + service_charge
+total = total -25
+print(total)
