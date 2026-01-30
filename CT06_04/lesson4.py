da0063a92fc0ddec777385b26295810216a34d04
Computhink-CS141 <cs141@computhink.com.sg>
2026-01-30 11:02:41 +0000

    end of lesson 4

--- CT06_04/lesson4.py
+++ CT06_04/lesson4.py
@@ -1,1 +1,49 @@
-print("Hello from lesson 4")
+# print("Hello from lesson 4")
+
+# total = (1*3) + (5*2) + (4*3)
+# print("$" + str(total))
+# print("$", total)
+
+# yourname = input("what is your name?") 
+# ask a question
+# print("Nice to meet you, " + yourname)
+# string concatenation
+
+# colour = input("what is your favourite colour?")
+# print("I love " + colour + " too!")
+
+# age = input("what is your age?")
+# print(age)
+
+# name = "..."
+# message = "//"
+# print(name + " says " + message)
+# # string concatenation
+# print( 2 + 22 , "what is this?")
+# # Math addition operator
+
+# name = input("what is  your name?")
+# print("hi, " + name + "!")
+# hobby = input("whats your favourite hobby?")
+# print("i enjoy " + hobby)
+# dream = input("what is your dream vacation destination?")
+# print("I would love to visit " + dream)
+
+# qty = 1
+# amt = 17.95
+# print("total cost: $" , qty * amt)
+
+num1 = 112
+num2 = 5
+ans= num1 + num2
+
+print(str(num1) , " + " , str(num2) + " = " , str(ans))
+
+age = input("what is your age? ")
+print("Your age next year is: " + str(int(age) + 1))
+
+num = input("give me a number")
+print("double your number is: " + str(int(num) * 2))
+
+yr = input("what year were you born in?")
+print("you are " + str(int(num) - 2026) + " years old")
