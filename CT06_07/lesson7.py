7dc26ab2582aaab5c81b7c13f98b22d6b46d5f07
Computhink-CS141 <cs141@computhink.com.sg>
2026-03-01 04:39:35 +0000

    end of lesson 7

--- CT06_07/lesson7.py
+++ CT06_07/lesson7.py
@@ -1,2 +1,45 @@
 print("Hello from lesson 7")
 
+# score_one = 80
+# score_two = 90
+# score_three = 75
+# total = score_one + score_two + score_three
+# average_score = total / 3
+# student_name = "Alex"
+# print("Average score for ", student_name, " is: ", round(average_score, 2))
+
+# for count in range(1, 11):
+#     print(count)
+# for count in range(2, 21 , 2):
+#     print(count)
+# for count in range(1, 11, -1):
+#     print(count)
+
+# word = input("what word do you want to repeat? ")
+# n = input("How many times would you like to repeat? ")
+# n = int(n)
+# for count in range( n ):
+#     print(word)
+
+# name = input("What is your name? ")
+# n = input("How many times would you like to repeat? ")
+# n = int(n)
+# for count in range( n ):
+#     print("nice to meet you, ", name)
+
+# n1 = input("what is number #1?")
+# n2 = input("what is number #2?")
+# n3 = input("what is number #3?")
+# n4 = input("what is number #4?")
+# n5 = input("what is number #5?")
+# n1 = int(n1)
+# n2 = int(n2)
+# n3 = int(n3)
+# n4 = int(n4)
+# n5 = int(n5)
+# total = 
+# print("sum of the 5 numbers is ")
+
+anum = int(  input("which times table?"))
+for count in range(1, 13):
+    print(anum, "x", count, "=", str(anum*count))
