5717be8bb5e55dfc2caa758c2f3b44e42dc4899a
Computhink-CS141 <cs141@computhink.com.sg>
2026-03-29 04:30:29 +0000

    end of lesson 10

--- CT06_09/lesson9.py
+++ CT06_09/lesson9.py
@@ -10,13 +10,23 @@ print("Hello from lesson 9")
 # all_even_odd = (num1%2==0) == (num2%2==0) == (num3%2==0)
 # print("all numbers are even/odd: " , all_even_odd)
 
+costApple = 0.6
+costOrange = 0.9
 numApple = int(input("how many apples? "))
 numOrange = int(input("how many oranges? "))
 if numApple > 5:
-    costApple = numApple * 0.6 * 0.9
+    costApple = (numApple * costApple) - 90/100 (numApple * costApple)
 else:
     costApple = numApple * 0.6
 if numOrange > 5:
     costOrange = numOrange * 0.9 * 0.9
 else:
     costOrange = numOrange * 0.9
+
+# costApple = 1
+# numApple = int(input("how many apples?"))
+# if numApple > 10:
+#     costApple = (numApple * costApple) - 10/100 * (numApple * costApple)
+# else:
+#     costApple = numApple * costApple
+# print(costApple)
--- CT06_10/lesson10.py
+++ CT06_10/lesson10.py
@@ -1,1 +1,37 @@
-print("Hello from lesson 10")
+# print("Hello from lesson 10")
+# guess = int(input("input a number "))
+# if guess > 0:
+#     print(guess , "is positive")
+# else:
+#     print(guess , "is negative")
+
+# age = int(input("what is the age? "))
+# if 13 < age < 19:
+#     print("teen")
+# else:
+#     if 13 > age:
+#         print("child")
+#     else:
+#         print("adult")
+
+# temperature = int(input("temperature? "))
+# if temperature > 30:
+#     print("go swimming")
+# elif 25 <= temperature:
+#     print("go play basketball")
+# elif 20 <= temperature:
+#     print("go cycling")
+# else:
+#     print("read indoors")
+
+score = int(input("what is the score "))
+if 90 < score < 100:
+    print("A")
+elif 80 < score < 89:
+    print("B")
+elif 70 < score < 79:
+    print("C")
+elif 60 < score < 69:
+    print("D")
+else:
+    print("F")
