f4930c6e3c7226ab2115798cb1b566d0725210f9
Computhink-CS141 <cs141@computhink.com.sg>
2026-04-05 04:31:21 +0000

    end of lesson 11

--- CT06_11/lesson11.py
+++ CT06_11/lesson11.py
@@ -1,1 +1,58 @@
 print("Hello from lesson 11")
+
+# px = int(input("what is the price? "))
+# if px <= 5:
+#     print("sounds good!")
+# elif px <= 50:
+#     print("are you sure you need this?")
+# elif px <= 500:
+#     print("where are you getting this money from?")
+# else:
+#     print("don't even think about it!")
+
+# rider_1 = 125
+# rider_2 = 150
+# if rider_1 and rider_2 >= 120:
+#     print("allow")
+# else:
+#     print("do not allow")
+
+# num1 = int(input("input a number "))
+# divisibleBy3 = num1 % 3 == 0
+# divisibleBy7 = num1 % 7 == 0
+# if divisibleBy3 and divisibleBy7:
+#     print("the number is divisible by 3 and 7")
+# elif divisibleBy3:
+#     print("the number is only divisible by 3")
+# elif divisibleBy7:
+#     print("the number is only divisible by 7")
+# else:
+#     print("the number is not divisible by 3 and 7")
+
+# rider1 = 25
+# rider2 = 6
+# if (rider1 >= 18) or (rider2 >= 18):
+#     print("please go ahead")
+# else:
+#     print("bring an adult with you")
+
+# age = int(input("what is your age? "))
+# if (age <= 12) or (age >= 65):
+#     print("ticket price: $15")
+# else:
+#     print("ticket price: $20")
+
+# ans = int(input("input gender (Male/Female/M/F): "))
+# if (ans = M) or (ans = Male):
+#     print("valid input")
+# else:
+#     print("invalid input")
+
+# ans = int(input("name a colour: "))
+# isGreen = ans == "Green"
+# if not isGreen:
+#     print("try again")
+# else:
+#     print("its green")
+
+ans = int(input(""))
